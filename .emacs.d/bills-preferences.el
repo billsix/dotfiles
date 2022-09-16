f1f30cae48c02c55c1286b36ff0b19ee3a3c7b5f
William Emerison Six <billsix@gmail.com>
2022-09-15 20:48:01 -0400

    emacs config dir, this way I can make python mode or cpp mode seperate

---
+++ .emacs.d/bills-preferences.el
@@ -0,0 +1,15 @@
+(set-background-color "black")
+(set-foreground-color "white")
+(show-paren-mode)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(setq inhibit-startup-message t)        ; Disable startup message
+(setq-default indent-tabs-mode nil)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+
+(visual-line-mode t)
+(global-visual-line-mode t)
+(global-linum-mode t)
+(set-buffer-file-coding-system 'unix)
---
+++ .emacs.d/init.el
@@ -0,0 +1,10 @@
+(load-file "bills-preferences.el")
+(load-file "helm.el")
+;; type C-x-e after the sexepression to execute
+;; (load-file "install-melpa-packages.el")
+;; (load-file "c++-mode.el")
+;; (load-file "python-mode.el")
+
+;; theme
+(load-theme 'material t)
+;;(load-theme 'the-matrix t)
