572b0dfea5bae477e8da115b5b30af4e89cc4b96
William Emerison Six <billsix@gmail.com>
2022-09-18 16:05:16 -0500

    load files given their full path

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,9 @@
-(load-file "bills-preferences.el")
-(load-file "helm.el")
+(load-file "~/.emacs.d/bills-preferences.el")
+(load-file "~/.emacs.d/helm.el")
 ;; type C-x-e after the sexepression to execute
-;; (load-file "install-melpa-packages.el")
-;; (load-file "c++-mode.el")
-;; (load-file "python-mode.el")
+;; (load-file "~/.emacs.d/install-melpa-packages.el")
+;; (load-file "~/.emacs.d/c++-mode.el")
+;; (load-file "~/.emacs.d/python-mode.el")
 
 ;; theme
 (load-theme 'material t)
