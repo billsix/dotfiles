7e51499aee16aef8315c044815bd3e942555ab07
William Emerison Six <billsix@gmail.com>
2022-12-30 17:35:06 -0500

    autorevert

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (load-file "~/.emacs.d/bills-preferences.el")
 (load-file "~/.emacs.d/helm.el")
 ;; type C-x-e after the sexepression to execute
@@ -8,3 +15,5 @@
 ;; theme
 (load-theme 'material t)
 ;;(load-theme 'the-matrix t)
+(global-auto-revert-mode)
+(setq auto-reve9rt-avoid-polling t)
