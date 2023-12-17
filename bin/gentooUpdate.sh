28aa917829943c65402c19a8cc3405956111512b
William Emerison Six <billsix@gmail.com>
2023-12-17 10:34:49 -0500

    learn stockfish

--- bin/gentooUpdate.sh
+++ bin/gentooUpdate.sh
@@ -6,9 +6,9 @@ emerge --sync
 git add -A .
 git commit -m 'updated'
 emerge --oneshot -uv sys-apps/portage
-emerge -uDNv  @world
+# emerge -uDNv  @world
 #emerge -uUD --backtrack=100 @world
 #emerge --depclean
-#emerge -uv @world
-emerge -uv @preserved-rebuild
+emerge -uv @world
+#emerge -uv @preserved-rebuild
 popd
