01659841f722bbb63ab2dd8ea2de64de3e0ceb5c
William Emerison Six <billsix@gmail.com>
2024-02-10 21:13:19 -0500

    updated shows

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,7 +25,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yafolding which-key the-matrix-theme projectile org-modern material-theme markdown-preview-mode magit helm-xref helm-lsp fold-this flycheck elpy discover dap-mode blacken better-defaults)))
+   '(ack ellama evil-escape theme-buffet ess dired-duplicates drepl devicetree-ts-mode dape yafolding which-key the-matrix-theme projectile org-modern material-theme markdown-preview-mode magit helm-xref helm-lsp fold-this flycheck elpy discover dap-mode blacken better-defaults)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
