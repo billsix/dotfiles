df065515aeac10d4b8cfb4635698270ab6ade7df
William Emerison Six <billsix@gmail.com>
2025-04-17 17:29:58 -0400

    updated

--- bin/gentooUpdate.sh
+++ bin/gentooUpdate.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-pushd .
-cd /var/db/repos/gentoo/
 emerge --sync
-git add -A .
-git commit -m 'updated'
 emerge --oneshot -uv sys-apps/portage
-# emerge -uDNv  @world
-#emerge -uUD --backtrack=100 @world
-#emerge --depclean
 emerge -uv @world
-#emerge -uv @preserved-rebuild
-popd
+# emerge -uDNv  @world
+emerge -uv @preserved-rebuild
+emerge --depclean
