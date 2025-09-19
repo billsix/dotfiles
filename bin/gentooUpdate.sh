f572ff2363992302e8d69fbc26cadfef87361303
William Emerison Six <billsix@gmail.com>
2025-09-18 21:00:48 -0400

    chromium builds take too long

--- bin/gentooUpdate.sh
+++ bin/gentooUpdate.sh
@@ -2,8 +2,8 @@
 
 emerge --sync && \
 emerge --oneshot -uv sys-apps/portage && \
-emerge -uv --keep-going y @world && \
-emerge -uDNv --keep-going y @world && \
+emerge -uv --keep-going y --exclude www-client/chromium --exclude www-client/firefox @world && \
+emerge -uDNv --keep-going y --exclude www-client/chromium --exclude www-client/firefox @world && \
 emerge -uv @preserved-rebuild && \
 emerge --depclean
 emerge -v @preserved-rebuild
