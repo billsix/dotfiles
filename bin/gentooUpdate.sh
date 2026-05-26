06ca8b9ef021a071c852afa982d2ce72c8ba6677
William Emerison Six <billsix@gmail.com>
2026-05-26 19:27:07 -0400

    for some reason, -auDNv caused issues for ffmpeg 8, but dropping -N solved it

--- bin/gentooUpdate.sh
+++ bin/gentooUpdate.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-emerge --sync && \
-emerge --oneshot -uv sys-apps/portage && \
-emerge -uv --keep-going y --exclude www-client/chromium --exclude www-client/firefox  --exclude dev-qt/qtwebengine @world && \
-emerge -uDNv --keep-going y --exclude www-client/chromium --exclude www-client/firefox --exclude dev-qt/qtwebengine @world && \
-emerge -uv @preserved-rebuild && \
-emerge --depclean
-emerge -v @preserved-rebuild
+emerge --sync
+emerge --oneshot -uv sys-apps/portage
+emerge -uv --keep-going y  @world
+emerge -uDv --keep-going y  @world
+emerge -uDNv --keep-going y  @world
+emerge -uv @preserved-rebuild
+# emerge --depclean
+# emerge -v @preserved-rebuild
